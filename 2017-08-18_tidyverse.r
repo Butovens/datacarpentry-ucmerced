c06c26e8062f7701aeb13c04fa3854990c8d6709
Butovens <bmede@ucmerced.edu>
2017-08-18 13:36:42 -0700

    Did new challenge

--- 2017-08-18_tidyverse.r
+++ 2017-08-18_tidyverse.r
@@ -27,9 +27,9 @@ surveys%>%
 
 ## Challenge
 ## Create a new data frame from the surveys data that meets the following criteria: contains only
-## the species_id column and a mew column called 
-
-##tha are half the hindfoot_length values. In this hindfoot_half 
+## the species_id column and a mew column called hindfoot_half containing values that are half 
+## the hindfoot_length values. In this hindfoot_half column, there are no NAs and all values are 
+## less than 30.
 
 surveys_hindfoot <- surveys %>%
   mutate(hindfoot_half = hindfoot_length / 2) %>%
@@ -56,4 +56,49 @@ surveys_hindfoot <- surveys %>%
  surveys %>%
    group_by(sex) %>%
    tally
+ 
+ ## Challenge
+ 
+ ## 1. How many individuals were caught in each plot_type surveyed?
+ surveys%>%
+   group_by(plot_type)%>%
+   tally
+ 
+ ## 2. Use group_by() and summarize to find the mean, min, and max hindfoot for each species 
+ ## (using species_id)
+ surveys%>%
+   group_by(species_id)%>%
+   filter(!is.na(hindfoot_length))%>%
+   summarize(mean_hindfoot= mean(hindfoot_length),
+             min_hindfoot= min(hindfoot_length),
+             max_hindfoot= max(hindfoot_length))
+ 
+ ## 3. What was the heaviest animal measured in each year? Return the columns year, species_id 
+ ## and weight
+ surveys%>%
+   select(year, genus, species_id, weight)%>%
+   group_by(year)%>%
+   top_n(1,weight)%>%
+   arrange(year)
+ 
+ ## answer 2
+ surveys %>%
+   filter(!is.na(weight))%>%
+   group_by(year)%>%
+   filter(weight== max(weight))%>%
+   select(year,genus,species,weight)%>%
+   arrange(year)
+   
+ 
+ ## 4. You saw above how to count the number of individuals of each sex using a
+ ## combination of group_by() and tally(). How could you get the same result using
+ ## group_by() and summarize()? Hint: see ?n.
+   surveys %>%
+   group_by(sex)%>%
+   summarise(n())
+ 
+
+ 
+
+   
  
